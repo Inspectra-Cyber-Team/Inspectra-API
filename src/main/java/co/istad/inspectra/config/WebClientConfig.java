f511970900a9ab3e92391b0abf0e17cd8a0eff39
Lyhou Phiv <lyhou282@gmail.com>
2024-12-03 11:08:33 +0700

    add cros origin

--- src/main/java/co/istad/inspectra/config/WebClientConfig.java
+++ src/main/java/co/istad/inspectra/config/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebClientConfig  {
+public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
@@ -19,19 +19,6 @@ public class WebClientConfig  {
 
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NotNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
 
 
 }
